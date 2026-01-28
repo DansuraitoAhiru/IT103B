fff5a5526f9744a7ddb4449aef77339fb32c0d1d
DansuraitoAhiru <vqviet2007@gmail.com>
2026-01-28 22:37:14 +0700

    Update baiTH.js

--- SS1/BTSP/baiTH.js
+++ SS1/BTSP/baiTH.js
@@ -2,7 +2,7 @@ let bookName=prompt("Nhập tên sách: ");
 let authorName=prompt("Nhập tên tác giả: ");
 let year=Number(prompt("Nhập năm xuất bản: "));
 const now = new Date().getFullYear();
-let price=prompt("Nhập giá sách: ");
+let price=Number(prompt("Nhập giá sách: "));
 let totalBooks=Number(prompt("Nhập số lượng: "));
 let authorCode=authorName.trim().toUpperCase().slice(0, 3);
 
@@ -13,4 +13,5 @@ console.log(`Tác giả: ${authorName.toUpperCase()}`);
 console.log(`Năm xuất bản: ${year}`);
 console.log(`Tuổi sách: ${now-year}`);
 console.log(`Tổng giá trị: ${price*totalBooks}`);
+
 console.log(`Ngăn kệ gợi ý: Kệ số ${Math.floor(Math.random()*10+1)}`);
