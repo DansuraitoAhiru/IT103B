978f8f79bdec15e30b687b30d7f7e29702765375
DansuraitoAhiru <vqviet2007@gmail.com>
2026-01-28 23:37:20 +0700

    Format and clean up baiTH.js code

--- SS1/BTSP/baiTH.js
+++ SS1/BTSP/baiTH.js
@@ -7,11 +7,12 @@ let totalBooks=Number(prompt("Nhập số lượng: "));
 let authorCode=authorName.trim().toUpperCase().slice(0, 3);
 
 console.log(`--- PHIẾU NHẬP KHO ---`);
-console.log(`Mã sách: ${authorCode}${year}-${(Math.floor(Math.random()*1000+1))}`);
+console.log(`Mã sách: ${authorCode}${year}-${(Math.floor(Math.random()*1000))+1}`);
 console.log(`Tên sách: ${bookName.toUpperCase()}`);
 console.log(`Tác giả: ${authorName.toUpperCase()}`);
 console.log(`Năm xuất bản: ${year}`);
 console.log(`Tuổi sách: ${now-year}`);
 console.log(`Tổng giá trị: ${price*totalBooks}`);
 
-console.log(`Ngăn kệ gợi ý: Kệ số ${Math.floor(Math.random()*10+1)}`);
+console.log(`Ngăn kệ gợi ý: Kệ số ${Math.floor(Math.random()*10)+1}`);
+
