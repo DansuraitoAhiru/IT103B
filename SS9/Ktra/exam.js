d6e20aac23d31bd3e2fd3aabbbd2da555e17cdd8
DansuraitoAhiru <vqviet2007@gmail.com>
2026-02-25 07:21:38 +0700

    Improve logging for order revenues
    Refactor console logs for clarity and remove redundancy.

--- SS9/Ktra/exam.js
+++ SS9/Ktra/exam.js
@@ -2,9 +2,9 @@ let orders=["Đơn hàng A", "Đơn hàng B", "Đơn hàng C", "Đơn hàng D", 
 let revenues=[1500, 2800, 1200, -500, 3200];
 
 const negativeNumber=revenues.some((element) => element < 0);
+console.log(`- Có đơn hàng âm: ${negativeNumber}`);
 const largerThan500=revenues.every((revenue) => revenue > 500);
-console.log(`- Có đơn hàng âm: ${negativeNumber ? true : false}
-- Tất cả trên 500: ${largerThan500 ? true : false}`);
+console.log(`- Tất cả trên 500: ${largerThan500}`);
 
 const netProfits=revenues.map((money) => money * 0.9);
 console.log(`Giá các đơn hàng sau khi giảm:`);
