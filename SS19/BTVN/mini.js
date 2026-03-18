ecaa67584fbcfa8c446fa55e40aa031de76d0afe
DansuraitoAhiru <vqviet2007@gmail.com>
2026-03-18 16:58:56 +0700

    Update mini.js

--- SS19/BTVN/mini.js
+++ SS19/BTVN/mini.js
@@ -1,4 +1,4 @@
-const products = [
+let products = [
   {
     id: 1,
     name: "Tai nghe Bluetooth TWS",
@@ -49,9 +49,225 @@ const products = [
   },
 ];
 
+let cart = [];
+
 const clearBtn = document.querySelector("#clear-cart-btn");
 
+const STORAGE_KEY = "cart";
 
-const renderProduct=() => {
+const saveData = () => {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(cart));
+};
 
+const loadData = () => {
+  const data = localStorage.getItem(STORAGE_KEY);
+
+  if (data) {
+    cart = JSON.parse(data);
+    cart = cart.filter((c) => products.some((p) => p.id === c.id));
+  } else {
+    cart = [];
+  }
 }
+
+const init = () => {
+  loadData();
+  renderProduct();
+  renderCart();
+  report();
+}
+
+const formatVND = (price) => {
+  return price.toLocaleString("vi-VN") + " VNĐ";
+}
+
+const renderProduct = () => {
+  let productList = document.querySelector("#products-grid");
+  const emptyState = document.querySelector("#products-empty");
+  if (products.length === 0) {
+    productList.innerHTML = "";
+    emptyState.style.display = "block";
+    return;
+  }
+
+  emptyState.style.display = "none";
+
+  productList.innerHTML = products.map((p) => `<article class="card">
+                                                    <div class="card-img">
+                                                        <img
+                                                        src=${p.image}
+                                                        alt=${p.name}
+                                                        loading="lazy"
+                                                        />
+                                                    </div>
+                                                    <div class="card-body">
+                                                        <h3 class="card-title">${p.name}</h3>
+                                                        <p class="card-desc">${p.description}</p>
+                                                        <div class="card-footer">
+                                                        <div class="price">${formatVND(p.price)}</div>
+                                                        <button onclick="addToCart(${p.id})" class="btn btn-primary">Thêm vào giỏ</button>
+                                                        </div>
+                                                    </div>
+                                                </article>
+    `).join("");
+};
+
+const addToCart = (id) => {
+  let item = cart.find((p) => p.id === id);
+  if (item) {
+    item.quantity++;
+  } else {
+    cart.push({ id: id, quantity: 1 });
+  }
+  saveData();
+  renderCart();
+  report();
+};
+
+const renderCart = () => {
+  let listCartItem = document.querySelector("#cart-tbody");
+  let emptyState = document.querySelector("#cart-empty");
+
+  if (cart.length === 0) {
+    listCartItem.innerHTML = "";
+    emptyState.style.display = "block";
+    return;
+  }
+
+  emptyState.style.display = "none";
+
+  listCartItem.innerHTML = cart.map((c) => {
+    let item = products.find((p) => p.id === c.id)
+    if(!item) return;
+    return `<tr>
+                  <td>${item.name}</td>
+                  <td class="right">${formatVND(item.price)}</td>
+                 <td class="center">
+                  <div class="qty-controls">
+                    <button onclick="decrease(${c.id})" class="btn btn-icon btn-ghost">-</button>
+                    <span class="qty">${c.quantity}</span>
+                    <button onclick="increase(${c.id})" class="btn btn-icon btn-ghost">+</button>
+                  </div>
+                </td>
+                  <td class="right">${formatVND(item.price * c.quantity)}</td>
+                  <td class="center">
+                    <button onclick="removeCartItem(${c.id})" class="btn-danger btn">Xóa</button>
+                  </td>
+              </tr>
+        `;
+  }).join("");
+  saveData();
+};
+
+const increase = (id) => {
+  let item = cart.find((p) => p.id === id);
+
+  if (item) {
+    item.quantity++;
+    saveData();
+    renderCart();
+    report();
+  }
+};
+
+const decrease = (id) => {
+  let item = cart.find((p) => p.id === id);
+  if (item) {
+    item.quantity--;
+
+    if (item.quantity <= 0) {
+      cart = cart.filter((p) => p.id !== id);
+    }
+
+    saveData();
+    renderCart();
+    report();
+  }
+};
+
+const removeCartItem = (id) => {
+  const found = cart.find((p) => p.id === id);
+  if (!found) return;
+
+  const product = products.find((p) => p.id===id)
+
+  Swal.fire({
+    title: "Are u sure?",
+    text: `Bạn muốn xóa ${product.name}?`,
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonText: "Chan đê",
+    cancelButtonText: "Hủy",
+    confirmButtonColor: "#e74c3c",
+    cancelButtonColor: "#6c757d"
+  }).then((result) => {
+    if (result.isConfirmed) {
+      cart = cart.filter((p) => p.id !== id);
+
+      saveData();
+      renderCart();
+      report();
+
+      Swal.fire({
+        title: "Đã xóa!",
+        text: "Sản phẩm đã bị xóa.",
+        icon: "success"
+      });
+    }
+  });
+};
+
+clearBtn.addEventListener("click", () => {
+  Swal.fire({
+    title: "WARNING",
+    text: `Bạn muốn xóa tất cả ko?`,
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonText: "Chan đê",
+    cancelButtonText: "Hủy",
+    confirmButtonColor: "#e74c3c",
+    cancelButtonColor: "#6c757d"
+  }).then((result) => {
+    if (result.isConfirmed) {
+      cart.length = 0;
+
+      saveData();
+      renderCart();
+      report();
+
+      Swal.fire({
+        title: "Đã xóa!",
+        text: "Tất cả sản phẩm đã bị xóa.",
+        icon: "success"
+      });
+    }
+  }); 
+});
+
+const report=() => {
+  const totalProducts = document.querySelector("#product-count-badge");
+  const totalCartLines = document.querySelector("#cart-lines-badge");
+  const totalCartQuantity = document.querySelector("#cart-qty-badge");
+  const totalLines = document.querySelector("#stat-lines");
+  const totalQuantity = document.querySelector("#stat-qty");
+  const totalValue = document.querySelector("#stat-total");
+
+  let totalP = products.length;
+  let totalCl = cart.length;
+  let totalCq = cart.reduce((prev, curr) => prev + curr.quantity, 0);
+  let totalL = cart.length;
+  let totalQ = cart.reduce((prev, curr) => prev + curr.quantity, 0);
+  let totalV = cart.reduce((prev, curr) => {
+    const product = products.find((p) => p.id === curr.id);
+    return prev + product.price * curr.quantity;
+  }, 0);
+
+  totalProducts.textContent = totalP + " sản phẩm";
+  totalCartLines.textContent = totalCl + " dòng";
+  totalCartQuantity.textContent = totalCq + " món";
+  totalLines.textContent = totalL;
+  totalQuantity.textContent = totalQ;
+  totalValue.textContent = formatVND(totalV);
+}
+
+init();
